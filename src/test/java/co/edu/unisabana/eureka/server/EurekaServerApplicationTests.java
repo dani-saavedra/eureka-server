9e20d5e05044387874b9a217c7305a6e5b906e9b
Dani Saavedra <daniel.saavedra@mercadolibre.com.co>
2022-10-26 19:06:56 -0500

    first commit

---
+++ src/test/java/co/edu/unisabana/eureka/server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unisabana.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
